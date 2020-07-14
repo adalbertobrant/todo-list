c196910f8e696d740218afc39195e022941b352b
Juliana Gaioso <juligaioso@users.noreply.github.com>
2020-07-13 22:51:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Tarefas</h1>
+      <input-task @newTask="addTask"></input-task>
+      <task-list v-bind:todo-list="tasks"></task-list>
+    </header>
+  </section>
+</template>
+
+<script>
+import InputTask from './components/InputTask'
+import TaskList from './components/TaskList'
+
+export default {
+  name: 'App',
+  components: {
+    InputTask,
+    TaskList
+  },
+  methods: {
+    addTask (task) {
+      this.tasks.push(task)
+    }
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <ul class="todo-list">
+ 
+        <li v-for="(todo, index) in sortedTasks" class="todo" :key="index">
+            <div clas="view">
+                <input class="toggle" @click="completeTask(todo)" type="checkbox">
+                <label v-bind:class="{ 'todo-completed': todo.completed }" >{{ todo.title }}</label>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['todoList'],
+    computed: {
+        sortedTasks: function () {
+        let sorted = this.todoList.slice()
+        return sorted.sort(function (a, b) {
+            if (a.title < b.title) return -1
+            if (a.title > b.title) return 1
+            return 0
+        })
+        }
+  }
+}
+</script>
